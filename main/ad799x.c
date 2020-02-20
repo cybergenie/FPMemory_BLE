f20a0cf05f56c150dfc859e1bcb61352f11d33c4
cybergenie <jinwei.li@outlook.com>
2020-02-20 17:02:05 +0800

    更新ad799驱动

--- main/ad799x.c
+++ main/ad799x.c
@@ -9,7 +9,7 @@ static void ad799x_convst_gpio_init(void)
     //设置为输出模式
     io_conf.mode = GPIO_MODE_OUTPUT;
     //你想设置的引脚
-    io_conf.pin_bit_mask = (1 << GPIO_AD7998_CONVST);
+    io_conf.pin_bit_mask = (1 << GPIO_AD799X_CONVST);
     //关闭下拉模式
     io_conf.pull_down_en = 0;
     //禁用牵引模式
@@ -22,13 +22,67 @@ static void ad799x_convst_gpio_init(void)
 esp_err_t ad799x_power_up(void)
 {
     esp_err_t ret;
-    ad7998_convst_gpio_init();
-    ret = gpio_set_level(GPIO_AD7998_CONVST, 1);    
+    ad799x_convst_gpio_init();
+    ret = gpio_set_level(GPIO_AD799X_CONVST, 1);
     if (ret == ESP_OK)
     {
         vTaskDelay(5 / portTICK_PERIOD_MS);
-        ret = gpio_set_level(GPIO_AD7998_CONVST, 0);
-    }    
+        ret = gpio_set_level(GPIO_AD799X_CONVST, 0);
+    }
     return ret;
 }
 
+static esp_err_t i2c_master_read_adc(i2c_port_t i2c_num, uint8_t *data_rd, size_t size)
+{
+    int ret;
+    if (size == 0)
+    {
+        return ESP_OK;
+    }
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    //----------------
+    esp_err_t temp = i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ESP_SLAVE_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, ad7998_config.addr_config_reg, 2, ACK_CHECK_EN);
+    i2c_master_write(cmd, ad7998_config.result_config, 2, ACK_CHECK_EN);
+    i2c_master_start(cmd);
+    i2c_master_write(cmd, ad7998_config.addr_result_geg, 2, ACK_CHECK_EN);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1)
+    {
+        i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t read_adc_data(ADC_Data *adc_data_rd)
+{
+    esp_err_t ret;
+    uint8_t data_rd[DATA_LENGTH]={};
+
+    ret = ad799x_power_up();
+
+    if(ret == ESP_OK)
+    {
+        ret = i2c_master_read_adc(I2C_MASTER_NUM,data_rd,sizeof(data_rd));
+
+        memcpy(adc_data_rd->data_sensor_1,data_rd,2);
+        memcpy(adc_data_rd->data_sensor_2,data_rd+2,2);
+        memcpy(adc_data_rd->data_sensor_3,data_rd+4,2);
+        memcpy(adc_data_rd->data_sensor_4,data_rd+6,2);
+        memcpy(adc_data_rd->data_sensor_5,data_rd+8,2);
+        memcpy(adc_data_rd->data_sensor_6,data_rd+10,2);
+        memcpy(adc_data_rd->data_sensor_7,data_rd+12,2);
+        memcpy(adc_data_rd->data_sensor_8,data_rd+14,2);
+
+    }
+
+}
--- main/ad799x.h
+++ main/ad799x.h
@@ -10,15 +10,53 @@
 #include "driver/gpio.h"
 
 #include "esp_system.h"
+#include "driver/i2c.h"
 
-#define GPIO_AD7998_CONVST CONFIG_GPIO_AD7998_CONVST
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
 
+#define DATA_LENGTH 16
+
+#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM) /*!< I2C port number for master dev */
+#define GPIO_AD799X_CONVST CONFIG_GPIO_AD799X_CONVST
+#define ESP_SLAVE_ADDR CONFIG_I2C_ADC_ADDRESS                 /*!< ESP32 slave address, you can set any 7bit value */
+#define WRITE_BIT I2C_MASTER_WRITE                            /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                              /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                                      /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                                     /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                                           /*!< I2C ack value */
+#define NACK_VAL 0x1
+
+
+typedef struct
+{
+    uint8_t data_sensor_1[2];
+    uint8_t data_sensor_2[2];
+    uint8_t data_sensor_3[2];
+    uint8_t data_sensor_4[2];
+    uint8_t data_sensor_5[2];
+    uint8_t data_sensor_6[2];
+    uint8_t data_sensor_7[2];
+    uint8_t data_sensor_8[2];
+
+} ADC_Data;
 
 
 typedef struct 
 {
-    bool convst;
+    // bool convst;
+    uint8_t addr_result_geg[2];
+    uint8_t result_config[2];
+    uint8_t addr_config_reg[2];
+    
 } ad7998_config_t;
+
+ad7998_config_t ad7998_config = {
+    // .convst = false,
+    .addr_result_geg = {0x00,0x00},     //
+    .result_config = {0x0F,0xF8},
+    .addr_config_reg = {0x00,0x02},
+};
 
 
 esp_err_t ad799x_power_up(void);
