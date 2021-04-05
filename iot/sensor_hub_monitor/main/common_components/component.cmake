10aef1fe68818ee9fa831db78aaf9c3b3778a73e
Li Jinwei <jinwei.li@outlook.com>
2021-04-05 21:58:00 +0800

    添加main_test

--- iot/sensor_hub_monitor/main/common_components/component.cmake
+++
@@ -1,7 +0,0 @@
-
-add_compile_options(-fdiagnostics-color=always)
-
-list(APPEND EXTRA_COMPONENT_DIRS                                 
-                                "$ENV{IOT_SOLUTION_PATH}/examples/common_components"                               
-                                )
-
